27842a9843c6a65b1af42a15e05485011c4ed767
Akshith_V <127883963+Tagcod3r@users.noreply.github.com>
2024-12-11 03:08:04 +0530

    port changed on index.js

--- Quiz/index.js
+++ Quiz/index.js
@@ -8,7 +8,7 @@ import pg from 'pg';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'Views'));
