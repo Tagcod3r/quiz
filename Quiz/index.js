6ad40a7d162729cecaeca13ef49014c92b451c38
Akshith <akkianki565@gmail.com>
2024-12-11 13:03:29 +0530

    updated clean code

--- Quiz/index.js
+++ Quiz/index.js
@@ -7,7 +7,6 @@ import pg from 'pg';
 import dotenv from 'dotenv';
 import 'dotenv/config';
 
-
 dotenv.config();
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
@@ -21,8 +20,7 @@ app.set('views',path.join(__dirname,'Views'));
 app.use(express.static(path.join(__dirname,'public')));
 app.use(bodyparser.urlencoded({extended:true}));
 
-
-const db= new pg.Client({
+const db = new pg.Client({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
@@ -32,63 +30,50 @@ const db= new pg.Client({
 
 db.connect();
 
-let CnC =[];
-db.query("SELECT * FROM capital",(err,res)=>{
-    if(err)
-    {
-      console.error("Error executing query", err.stack);
-    }else{
-        CnC=res.rows;
+let CnC = [];
+db.query("SELECT * FROM capital", (err, res) => {
+    if(err) {
+        console.error("Error executing query", err.stack);
+    } else {
+        CnC = res.rows;
     }
     db.end();
 });
 
-/*
-const CnC = {
-    "USA": "Washington, D.C.",
-    "France": "Paris",
-    "Germany": "Berlin",
-    "Japan": "Tokyo"
-};*/
-
-let score=0;
+let score = 0;
 app.get('/', (req, res) => {
-
-    score=0;
-
-    const index = Math.floor(Math.random()*CnC.length);
+    score = 0;
+    const index = Math.floor(Math.random() * CnC.length);
     const randomcountry = CnC[index].country;
     const capital = CnC[index].capital;
     let gameover = false;  
     let alert = '';
-    res.render('home', { country: randomcountry, capital: capital, score: score ,alertMessage: alert, gameOver:gameover });
+    res.render('home', { country: randomcountry, capital: capital, score: score, alertMessage: alert, gameOver: gameover });
 });
-
 
 app.post('/', (req, res) => {
     const userAnswer = req.body.capital.trim().toLowerCase();
     const correctAnswer = req.body.correctCapital.toLowerCase();
     
-    let gameover =false;
-    let updatescore =0;
+    let gameover = false;
+    let updatescore = 0;
     let alert = '';
     if (userAnswer === correctAnswer) {
         score++; 
-    }else if(userAnswer === ''){
-            score=0;
-    }else{
-        updatescore=score;
-        score=0;
-        gameover=true;
+    } else if (userAnswer === '') {
+        score = 0;
+    } else {
+        updatescore = score;
+        score = 0;
+        gameover = true;
         alert = 'Game Over! Your final score';
     }
-
 
     const randomIndex = Math.floor(Math.random() * CnC.length);
     const randomcountry = CnC[randomIndex].country; 
     const capital = CnC[randomIndex].capital;  
 
-    res.render('home', { country: randomcountry, capital: capital, score: score, alertMessage: alert, gameOver:gameover, finals:updatescore });
+    res.render('home', { country: randomcountry, capital: capital, score: score, alertMessage: alert, gameOver: gameover, finals: updatescore });
 });
 
 app.listen(port, () => {
